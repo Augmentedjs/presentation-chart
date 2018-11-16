9601e05e41f1d2a0169e40f55584ecf5b7127c64
thedocbwarren <jedi.master@augmentedjs.com>
2018-11-15 16:52:05 -0800

    fix constructor

--- src/collections/data.js
+++ src/collections/data.js
@@ -3,12 +3,12 @@ import { Collection } from "presentation-models";
 import Point from "../models/point.js";
 
 class ChartData extends Collection {
-  constructor(options) {
+  constructor(models, options) {
     if (!options) {
       options = {};
     }
     options.model = Point;
-    super(options);
+    super(models, options);
   };
 };
 
